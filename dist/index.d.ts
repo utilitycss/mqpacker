5e8e122823e29a81d67c639908e878c9aa1f6d34
Sylvester <me@sylvesteraswin.com>
2021-02-28 21:19:39 +0100

    Remove node_modules

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import { Plugin } from "postcss";
-declare type SortCompareFunction<T = string> = (a: T, b: T) => number;
-declare type Sort = boolean | SortCompareFunction;
-interface PluginOptions {
-    sort?: Sort;
-}
-declare const plugin: (opts: PluginOptions) => Plugin;
-export default plugin;
-export declare const postcss = true;
